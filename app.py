843928073f9fbeee948a37d0e25f285844fb499f
SeHwan_Park <realsehwan@gmail.com>
2021-11-23 14:44:10 +0900

    add gitmodules

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import json
 
 from flask import Flask, render_template, request, jsonify
-from just import foo
+from counting.star import stars
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def query():
     data = json.loads(request.get_data())
+    print(stars())
     return jsonify({"result" : data})
 
 @app.route('/')
---
+++ counting/star.py
@@ -0,0 +1,6 @@
+import time
+
+
+def stars():
+    time.sleep(10)
+    return 1000;
